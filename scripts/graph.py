43f72b2a047444bbc43ad33e6e061080e8e28eb8
juan_mata <juan.mnaranjo@hotmail.com>
2020-12-30 17:17:23 +0100

    Finish question 4. We have included the kaglers algo to estimate the exact edges needed to be removed. The final algorithm computes both max flow and kagler and tries to compare them. If there is any divergence this is also mentioned

--- scripts/graph.py
+++ scripts/graph.py
@@ -1,11 +1,14 @@
-from collections import defaultdict, Counter
-import functionality as fn
+from collections import defaultdict, Counter, deque
+import scripts.functionality as fn
 import plotly.express as plty
 import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 from collections import deque
+import copy
+import random
+import heapq
 
 
 class Graph:
@@ -17,8 +20,9 @@ class Graph:
         :attribute edges : dictionary of vertices (keys) to sets of vertices (values)
         :attribute induced_subgraph:
         """
-        self._edges = defaultdict(set)
-
+        self._adj_list = defaultdict(set)
+        self._residual_graph = defaultdict(set)
+        self._capacity = defaultdict(dict)
 
     @classmethod
     def from_dict(cls, dict_):
@@ -29,12 +33,12 @@ class Graph:
         :return : new Graph instance
         """
         g = cls()
-        g._edges.update({k:set(v) for k, v in dict_.items()})
-        vertices = list(g._edges.keys())
+        g._adj_list.update({k: set(v) for k, v in dict_.items()})
+        vertices = list(g._adj_list.keys())
         for v in vertices:
-            for u in g._edges[v]:
-                g.add_vertex(u)     # make sure that all the vertices of the graph are in the adjacency list:
-                                    # without this step, a sink node wouldn't appear in g._edges
+            for u in g._adj_list[v]:
+                g.add_vertex(u)  # make sure that all the vertices of the graph are in the adjacency list:
+                # without this step, a sink node wouldn't appear in g._edges
         return g
 
     @property
@@ -42,7 +46,7 @@ class Graph:
         """
         Read-only property, number of vertices in the graph
         """
-        return len(self._edges)
+        return len(self._adj_list)
 
     @property
     def n_edges_(self):
@@ -50,8 +54,8 @@ class Graph:
         Read-only property, number of edges in the graph
         """
         n = 0
-        for v in self._edges:
-            n += len(self._edges[v]) 
+        for v in self._adj_list:
+            n += len(self._adj_list[v])
         return n
 
     @property
@@ -68,15 +72,23 @@ class Graph:
 
         :return : list of vertices
         """
-        return list(self._edges.keys())
+        return list(self._adj_list.keys())
 
-    def get_edges(self):
+    def get_edges(self, output='tuple'):
         """
         Get the edges of the graph
-
+        :param output: Output can be a list of tuples ('tuple'), a list of lists ('list') or a dictionary with lists
+            ('dict')
         :return : list of (source, destination) tuples
         """
-        return [(v, u) for v in self._edges.keys() for u in self._edges[v]]
+        if output == 'tuple':
+            return [(v, u) for v in self._adj_list.keys() for u in self._adj_list[v]]
+        else:
+            edge_list = [[v, u] for v in self._adj_list.keys() for u in self._adj_list[v]]
+            if output == 'list':
+                return edge_list
+            else:
+                return {i: edge for i, edge in enumerate(edge_list)}
 
     def add_edge(self, v, u):
         """
@@ -87,7 +99,7 @@ class Graph:
 
         :return : 
         """
-        self._edges[v].add(u)
+        self._adj_list[v].add(u)
 
     def add_vertex(self, v):
         """
@@ -97,8 +109,19 @@ class Graph:
         
         :return : 
         """
-        self._edges[v]
+        self._adj_list[v]
 
+    @staticmethod
+    def from_edges_to_graph(edges):
+        """
+        Given a list of tuples with vertices, generate a graph as a dict
+
+        :param edges: List of tuples with vertices
+        :return: Dict graph
+        """
+        dict_stupid = defaultdict(set)
+        for item in edges:
+            dict_stupid[item[0]].update([item[1]])
 
     def in_degree(self, v=None):
         """
@@ -108,18 +131,17 @@ class Graph:
         :return in_d : int
         """
         if v is None:
-            degrees = defaultdict.fromkeys(self._edges.keys(), 0)
+            degrees = defaultdict.fromkeys(self._adj_list.keys(), 0)
             for v in degrees:
-                for u in self._edges[v]:
+                for u in self._adj_list[v]:
                     degrees[u] += 1
             return dict(degrees)
         else:
             in_d = 0
-            for u in self._edges:
-                if v in self._edges[u]:
+            for u in self._adj_list:
+                if v in self._adj_list[u]:
                     in_d += 1
             return in_d
-
 
     def out_degree(self, v=None):
         """
@@ -129,12 +151,11 @@ class Graph:
         :return  : int
         """
         if v is None:
-            degrees = dict.fromkeys(self._edges.keys(), 0)
+            degrees = dict.fromkeys(self._adj_list.keys(), 0)
             for v in degrees:
-                degrees[v] = len(self._edges[v])
+                degrees[v] = len(self._adj_list[v])
             return degrees
-        return len(self._edges[v])
-
+        return len(self._adj_list[v])
 
     def degree(self, v=None):
         """
@@ -146,12 +167,11 @@ class Graph:
         if v is None:
             in_deg = self.in_degree()
             out_deg = self.out_degree()
-            degrees = dict.fromkeys(self._edges.keys(), 0)
+            degrees = dict.fromkeys(self._adj_list.keys(), 0)
             for v in degrees:
                 degrees[v] = in_deg[v] + out_deg[v]
             return degrees
         return self.in_degree(v) + self.out_degree(v)
-
 
     def degree_distro(self, normalize=False):
         """
@@ -163,10 +183,9 @@ class Graph:
         degree_dist = Counter(self.degree().values())
         if normalize:
             n_v = self.n_vertices_
-            degree_dist = {d:(degree_dist[d] / n_v) for d in degree_dist}
+            degree_dist = {d: (degree_dist[d] / n_v) for d in degree_dist}
         return dict(degree_dist)
 
- 
     def plot_degree_distro(self, normalize=True):
         """
         Plot the degree distribution of the graph
@@ -175,9 +194,49 @@ class Graph:
         :return 
         """
         degree_dist = self.degree_distro(normalize)
-        fig = plty.histogram(data_frame=pd.DataFrame(data=degree_dist.items(), columns=['Degree', 'Normalized number of nodes']),\
-                            x='Degree', y='Normalized number of nodes', title='Degree distribution')
+        fig = plty.histogram(
+            data_frame=pd.DataFrame(data=degree_dist.items(), columns=['Degree', 'Normalized number of nodes']), \
+            x='Degree', y='Normalized number of nodes', title='Degree distribution')
         fig.show()
+
+    # TODO: How can we make this function not static (graph changes constantly and don't want to chaneg the classes
+    #  attribute)
+    @staticmethod
+    def get_random_edge(edges, source, sink):
+        """
+        Function that randomly returns two connected vertices
+
+        :param edges: List of list containing the edges over which to pick
+        :param source & sink: These are the two vertices which we want to disconnect, therefore they cannot be v2
+        (v2 is the node that will disappear so to say)
+        :return: two vertices
+        """
+
+        v1, v2 = random.choice(list(edges.values()))
+        while (v2 == source) | (v2 == sink):
+            v1, v2 = random.choice(list(edges.values()))
+
+        return v1, v2
+
+    @staticmethod
+    def contract_node(edges, v1, v2):
+        """
+        Function to contract two nodes into one. The logic is that we will delete the edge between two nodes, and
+        basically merge the node v2 into v1 (v1 will inherit all v2's edges).
+        """
+
+        # Make v1 inherit all of v2's edges
+        contracted = {}
+        for k, v in edges.items():
+            if v2 in v:
+                contracted[k] = [v1 if v2 == ele else ele for ele in v]
+            else:
+                contracted[k] = v
+
+        # Remove edges with same in and out node
+        contracted = {k: v for k, v in contracted.items() if v[0] != v[1]}
+
+        return contracted
 
     def __repr__(self):
         """
@@ -185,8 +244,7 @@ class Graph:
         """
         return str(self.get_edges())
 
-    @staticmethod
-    def plot_graph(graph, with_labels=True, node_size=100):
+    def plot_graph(self, with_labels=True, node_size=100):
         """
         Method to visualize graph or sub_graph
 
@@ -195,12 +253,13 @@ class Graph:
         :param node_size: node size
         :return: Plot
         """
-        g = nx.DiGraph(graph)
+        g = nx.DiGraph(self._adj_list)
         plt.figure(figsize=(12, 8))
         plt.clf()
         nx.draw(g, with_labels=with_labels, node_size=node_size)
         plt.show();
 
+    # Question 2
     def pages_in_click(self, initial_page, num_clicks, print_=False):
         """
         Given a graph, an initial starting point and the number of clicks, how many, and which pages will we be able to
@@ -231,8 +290,8 @@ class Graph:
                 if print_:
                     print(node)
                 # If a given node has target node, include the out-nodes into the new_queue list
-                if bool(self.edges[node]):
-                    new_queue.update(self.edges[node])
+                if bool(self._adj_list[node]):
+                    new_queue.update(self._adj_list[node])
                 # If a given node has no target node, include it in the last_nodes list (this list will not be used to)
                 # for further inspection but we will have to consider it as an article that has been seen
                 else:
@@ -252,9 +311,6 @@ class Graph:
         # Return the unique pages
         return set(pages_visited)
 
-    # TODO:  Use class methods to  construct new induced-subgraph. Issue is that the methods construct over the 
-    #  self.edge and we don't  want that
-    # TODO: See how to return the new class
     def generate_induced_subgraph(self, vertices):
         """
         Given a set of vertices, compute it's induced subgraph
@@ -264,9 +320,10 @@ class Graph:
         """
         induced_subgraph = defaultdict(set)
         for vertex in vertices:
-            induced_subgraph[vertex] = vertices.intersection(self.edges[vertex])
+            induced_subgraph[vertex] = vertices.intersection(self._adj_list[vertex])
+            induced_subgraph = {k: v for k, v in induced_subgraph.items() if v}
 
-        return Graph(induced_subgraph)
+        return Graph.from_dict(induced_subgraph)
 
     # TODO: probably not needed method
     def bfs(self, start):
@@ -283,26 +340,152 @@ class Graph:
             node = queue.popleft()
             if node not in visited:
                 visited.update([node])
-                neighbours = self.edges[node]
+                neighbours = self._adj_list[node]
                 for neighbour in neighbours:
                     queue.append(neighbour)
         return visited
 
-    def min_cut(self, article1, article2):
+    # Question 4
+    def build_residual_graph_capacity(self):
         """
-        Given two random articles, get the minimum number of edges that need to be breaken down to dut the link
-        between both articles.
+        The max-flow method requires a residual graph to work. This can also be seen as an attribute of the class,
+        therefore we will include it as a self.
 
-        This algorithm will follow the logic followed by the Edmonds-Karp Algorithm (max flow is equal to min cut),
-        which is an improvement on the Ford-Fulkerson Algorithm
-
-        :param article1: Integer representing an article (source article)
-        :param article2: Integer representing an article (sink article)
-        :return: Integer (number of edges)
+        :return: Generate attribute
         """
-        x = self.induced_subgraph
-        min_cut = 0
+        for k, v in self._adj_list.items():
+            self._residual_graph[k].update(v)
+            self._capacity[k].update({k: 1 for k in v})
+            for i in v:
+                self._residual_graph[i].update([k])
+                self._capacity[i].update({k: 0})
 
-        return min_cut
+    @staticmethod
+    def compute_augmented_path(graph, capacity, source, sink):
+        """
+        Function to compute the augmented paths required for the max flow algo (which is equal to the min cut value)
+        """
+        visited = set()
+        queue = deque([source])
+        parent_map = []
 
+        count = 0
+        while queue:
+            count += 1
+            node = queue.popleft()
+            if node not in visited:
+                visited.update([node])
+                neighbours = graph[node]
+                for neighbour in neighbours:
+                    if (neighbour not in visited) & (capacity[node][neighbour] > 0):
+                        queue.append(neighbour)
+                        parent_map.append([neighbour, node])
+                        if neighbour == sink:
+                            return parent_map
+                    else:
+                        continue
 
+    @staticmethod
+    def construct_flow(parent_map, sink):
+        """
+        Given a parent map (output of the compute augmented path function) contruct a flow
+
+        """
+        flow = [sink]
+        for row in parent_map[::-1]:
+            if row[0] == flow[-1]:
+                flow.append(row[1])
+        return flow[::-1]
+
+    @staticmethod
+    def compute_flow_adjust_capacity(flow, capacity):
+        """
+        Given a flow and the capacity values for each, compute the flow value (min over the given augmented path), and
+        re-adjust the capacity dictionary for the next iteration
+
+        """
+
+        flow_value = min([capacity[flow[i]][flow[i + 1]] for i in range(0, len(flow) - 1)])
+        for i in range(len(flow) - 1):
+            capacity[flow[i]][flow[i + 1]] = capacity[flow[i]][flow[i + 1]] - flow_value
+            capacity[flow[i + 1]][flow[i]] = capacity[flow[i + 1]][flow[i]] + flow_value
+
+        return flow_value, capacity
+
+    def max_flow_func(self, source, sink):
+        """
+        Function to compute the max flow over a given graph, capacity, initial source value and sink value
+
+        """
+        self.build_residual_graph_capacity()
+
+        max_flow = 0
+        capacity = copy.deepcopy(self._capacity)
+
+        while True:
+            augmented_path = self.compute_augmented_path(graph=self._residual_graph, capacity=capacity,
+                                                         source=source, sink=sink)
+            if not augmented_path:
+                return max_flow, capacity
+            flow = self.construct_flow(augmented_path, sink)
+            flow_value, capacity = self.compute_flow_adjust_capacity(flow, capacity)
+            max_flow += flow_value
+
+    # TODO: This method only seems to work when the edges are weighted
+    # @staticmethod
+    # def find_min_cut(initial_graph, residual_graph):
+    #     """
+    #     Following the max flow logic, we can compute the set of minimum cut egdes by taking those edges which initially had
+    #     some kind of weight (different to 0), and after the max flow computation ended up having a weight of zero.
+    #
+    #     :param initial_graph: Initial graph with the correct capacities
+    #     :param residual_graph: Graph after obtaining the max flow, which we will compare against the first
+    #     :param plot: Bool to plot the edges required to be cut
+    #     :return: Set of edges to be cut
+    #     """
+    #     min_cut_edges = []
+    #     for key in initial_graph:
+    #         for sub_key in initial_graph[key]:
+    #             if (initial_graph[key][sub_key] > 0) & (residual_graph[key][sub_key] == 0):
+    #                 min_cut_edges.append([key, sub_key])
+    #     return min_cut_edges
+
+    # TODO: implement the kagler method which simply takes a original graph and contracts the edges at random. It does
+    #  so until it only has two nodes. The nodes that edges that remain are the number of edges required to cut
+    #  (approximation). Since we know the "real" min cut value, we can do the iteration until both solutions converge
+    #  (if there is no convergence after 100 simulations we could return the minimum set of edges with a  warning
+    #  stating that the real min is something else)
+    def min_cut_kagler(self, source, sink, max_flow, iterations=20):
+        """
+        This method computes the exact edges that need to be removed in order to disconnect two pages. For this purpose
+        will help ourselves with the max_flow approach (which gives the exact number of edges that need to be deleted
+        for this to happen and also takes into account the fact that the graph is directed.
+        This function is just an approximation, but given that we have the real result we will try to match it
+        :param source: Article page we want to disconnect from sink
+        :param sink: Article page we want to disconnect from source
+        :param max_flow: Min Cut value
+        :param iterations: Number of iterations before we stop looking for set of edges
+        :return: Set of edges to delete
+        """
+
+        initial_edges = self.get_edges('dict')
+        champion_set = []
+
+        for iteration in range(iterations):
+            edges = copy.deepcopy(initial_edges)
+            while len(set(x for l in list(edges.values()) for x in l)) > 2:
+                v1, v2 = self.get_random_edge(edges, source, sink)
+                edges = self.contract_node(edges, v1, v2)
+            edges_candidate = list({k: initial_edges[k] for k, v in edges.items()}.values())
+            if iteration == 0:
+                heapq.heappush(champion_set, (-len(edges), edges_candidate))
+            else:
+                heapq.heappushpop(champion_set, (-len(edges), edges_candidate))
+            if len(edges) == max_flow:
+                result = list(heapq.heappop(champion_set)[0])
+                result[0] = -result[0]
+                return heapq.heappop(champion_set)
+            if iteration == iterations-1:
+                print('Max Flow and Kagler\'s algorithm do not converge')
+                print('The minimum number of edges required to remove to disconnect is: ', max_flow)
+                print('The best edges to do this are: ', heapq.heappop(champion_set)[1])
